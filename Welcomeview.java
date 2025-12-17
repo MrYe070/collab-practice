673f2b909d5aaf0341c741acbf547c5a41696016
SophiePearce122 <sp9aj09@limestone.on.ca>
2025-12-17 13:04:16 -0500

    add varibles

--- Welcomeview.java
+++ Welcomeview.java
@@ -2,7 +2,8 @@
 class Welcomeview {
     String title = "Welcom to my app";
     int visits =0;
-    
+    int game = 0 ;
+
     Welcomeview(){
         System.out.println("HI");
         System.out.println("Bye");
