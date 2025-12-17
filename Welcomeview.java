d653f73d2a5bfead0ad17929d871411024c85497
SophiePearce122 <sp9aj09@limestone.on.ca>
2025-12-17 12:57:34 -0500

    Added varible
    Added visits

--- Welcomeview.java
+++ Welcomeview.java
@@ -1,5 +1,7 @@
+
 class Welcomeview {
     Welcomeview(){
+        Int Visits =  0;
         System.out.println("HI");
         System.out.println("Bye");
         System.out.println("whats 5 + 5");
